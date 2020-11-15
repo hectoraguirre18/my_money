e3ad678ffe9c19227532b06658a1cdef65080c41
Hector Aguirre <a320793@uach.mx>
2020-11-15 00:37:42 -0700

    form validations in login screen

--- lib/custom/edittext.dart
+++ lib/custom/edittext.dart
@@ -3,14 +3,20 @@ import 'package:flutter/material.dart';
 class EditText extends StatelessWidget {
 
   final String labelText;
+  final Function(String) validator;
+  final bool obscureText;
 
-  EditText({this.labelText});
+  EditText({this.labelText, this.validator, this.obscureText});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 12),
       child: TextFormField(
+        style: TextStyle(
+          color: Colors.white
+        ),
+        obscureText: obscureText ?? false,
         decoration: InputDecoration(
           labelText: labelText,
           labelStyle: TextStyle(
@@ -22,7 +28,7 @@ class EditText extends StatelessWidget {
             borderRadius: BorderRadius.circular(24)
           ),
         ),
-        
+        validator: validator ?? (_) => null,
       ),
     );
   }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:mymoney/custom/button.dart';
 import 'package:mymoney/custom/edittext.dart';
 
+
 class LoginScreen extends StatelessWidget {
+
+  final formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,6 +15,7 @@ class LoginScreen extends StatelessWidget {
         child: Center(
           child: SingleChildScrollView(
             child: Form(
+              key: formKey,
               child: Padding(
                 padding: const EdgeInsets.all(32.0),
                 child: Column(
@@ -31,14 +36,35 @@ class LoginScreen extends StatelessWidget {
                           SizedBox(height: 32),
                           EditText(
                             labelText: 'Correo',
+                            validator: (String value) {
+                              if(value == null || value.isEmpty)
+                                return 'Este campo no puede estar vacío';
+                              if(!value.contains('@'))
+                                return 'Correo inválido';
+                              return null;
+                            },
                           ),
                           EditText(
                             labelText: 'Contraseña',
+                            obscureText: true,
+                            validator: (String value) {
+                              if(value == null || value.isEmpty)
+                                return 'Este campo no puede estar vacío';
+                              return null;
+                            },
                           ),
                           Button(
                             text: 'Ingresar',
                             onPressed: () {
                               //TODO Checar login y abrir siguiente pantalla
+                              if(formKey.currentState.validate()) {
+                                //login()
+                                Navigator.push(context, MaterialPageRoute(
+                                  builder: (context) => Scaffold(
+                                    appBar: AppBar(),
+                                  )
+                                ));
+                              }
                             },
                           ),
                           Padding(
@@ -46,18 +72,24 @@ class LoginScreen extends StatelessWidget {
                             child: Row(
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
-                                Text(
-                                  '¿Olvidaste tu\ncontraseña?',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    decoration: TextDecoration.underline,
+                                InkWell(
+                                  onTap: () => print('continuar'),
+                                  child: Text(
+                                    '¿Olvidaste tu\ncontraseña?',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      decoration: TextDecoration.underline,
+                                    ),
                                   ),
                                 ),
-                                Text(
-                                  '¿No tienes\nuna cuenta?',
-                                  style: TextStyle(
-                                    color: Colors.white,
-                                    decoration: TextDecoration.underline,
+                                InkWell(
+                                  onTap: () => print('continuar'),
+                                  child: Text(
+                                    '¿No tienes\nuna cuenta?',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      decoration: TextDecoration.underline,
+                                    ),
                                   ),
                                 ),
                               ],
@@ -66,11 +98,14 @@ class LoginScreen extends StatelessWidget {
                         ],
                       ),
                     ),
-                    Text(
-                      'Continuar\nsin cuenta',
-                      style: TextStyle(
-                        color: Colors.white,
-                        decoration: TextDecoration.underline,
+                    InkWell(
+                      onTap: () => print('continuar'),
+                      child: Text(
+                        'Continuar\nsin cuenta',
+                        style: TextStyle(
+                          color: Colors.white,
+                          decoration: TextDecoration.underline,
+                        ),
                       ),
                     )
                   ],
