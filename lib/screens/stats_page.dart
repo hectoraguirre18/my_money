106953fc3048c7e68e9e68ed11e38a97dc8f642a
Hector Aguirre <a320793@uach.mx>
2020-11-24 18:47:37 -0700

    work in progress message

--- lib/screens/stats_page.dart
+++ lib/screens/stats_page.dart
@@ -4,10 +4,23 @@ class StatsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text(
-        'StatsPage',
-        style: TextStyle(
-          color: Colors.white
+      child: Opacity(
+        opacity: 0.3,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              'assets/images/work_in_progress.png',
+              width: MediaQuery.of(context).size.width*0.5,
+            ),
+            SizedBox(height: 16),
+            Text(
+              'We\'re working to bring\nyou this functionality!',
+              style: TextStyle(
+                color: Colors.white,
+              ),
+            )
+          ],
         ),
       ),
     );
